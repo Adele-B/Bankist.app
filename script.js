0b2c4613c1c6484a851fa1bf445ea4296c304f24
Adele-B <72464825+Adele-B@users.noreply.github.com>
2021-08-14 12:31:42 +0100

    Implemented delete account option

--- script.js
+++ script.js
@@ -148,9 +148,9 @@ btnLogin.addEventListener('click', function (e) {
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
+    // Update UI
+    updateUI(currentAccount);
   }
-  // Update UI
-  updateUI(currentAccount);
 });
 
 btnTransfer.addEventListener('click', function (e) {
@@ -176,4 +176,24 @@ btnTransfer.addEventListener('click', function (e) {
     // Update UI
     updateUI(currentAccount);
   }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
 });
